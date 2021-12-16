81cb0bb8257e5adc3ce056aa549c309e9f095207
GulSam00 <gulsamcono@gmail.com>
2021-12-16 19:41:55 +0900

    Debug : 배포 환경 깨짐 현상.

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -2,18 +2,12 @@ import { createMedia } from '@artsy/fresnel';
 import {
   Button,
   Container,
-  Divider,
   Grid,
   Header,
-  Icon,
   Image,
   List,
-  Menu,
   Segment,
-  Sidebar,
-  Visibility,
 } from 'semantic-ui-react';
-import './MainContainer.scss';
 
 const { MediaContextProvider, Media } = createMedia({
   breakpoints: {
@@ -79,9 +73,6 @@ const ResponsiveContainer = ({ children }) => (
 );
 
 const MainContainer = () => {
-  const handlerClick = () => {
-    alert('미구현!');
-  };
   return (
     <ResponsiveContainer>
       <Segment style={{ padding: '8em 0em' }} vertical>
@@ -235,32 +226,3 @@ const MainContainer = () => {
 };
 
 export default MainContainer;
-
-// import { Container, Header, Message, Grid } from 'semantic-ui-react';
-// import { Link } from 'react-router-dom';
-// const MainContainer = () => {
-//   return (
-//     <>
-//       <Container style={{ margin: 20 }}>
-//         <Header as="h1" dividing>
-//           우리는 도랑의 시대에 살고 있다!
-//         </Header>
-//         <Message info>이 문서의 내용은 한치의 거짓도 없는 사실입니다!</Message>
-//       </Container>
-//       <Grid celled>
-//         <Grid.Row>
-//           <Grid.Column width={3}></Grid.Column>
-//           <Grid.Column width={9}></Grid.Column>
-//         </Grid.Row>
-//         <Grid.Row>
-//           <Grid.Column width={3}></Grid.Column>
-//           <Grid.Column width={9}></Grid.Column>
-//         </Grid.Row>
-//       </Grid>
-//       <Link to="about">about</Link>
-//       <Link to="picture">picture</Link>
-//     </>
-//   );
-// };
-
-// export default MainContainer;
